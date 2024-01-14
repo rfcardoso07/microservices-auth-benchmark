221d1dbb7c04b3f0215de9c26817ead0fcbc6f58
rfcardoso07 <rafael.cardoso@inf.ufrgs.br>
2024-01-14 13:17:05 -0300

    Add gateway auth

---
+++ gateway/database.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type userPermissions struct {
+	CanRead   bool
+	CanWrite  bool
+	CanDelete bool
+}
+
+type database struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	DB       *sql.DB
+}
+
+func (d *database) init() error {
+	// Create connection string
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		d.Host, d.Port, d.User, d.Password, d.Name)
+
+	// Open a database connection and set up a connection pool
+	var err error
+	d.DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Printf("Error opening database connection: %v", err)
+		return err
+	}
+
+	// Set the maximum number of open (in-use + idle) connections
+	d.DB.SetMaxOpenConns(10)
+
+	// Set the maximum number of idle connections in the pool
+	d.DB.SetMaxIdleConns(5)
+
+	// Check if the database connection is alive
+	err = d.DB.Ping()
+	if err != nil {
+		log.Printf("Error pinging database: %v", err)
+		return err
+	}
+
+	log.Println("Connected to the database")
+	return nil
+}
+
+func (d database) searchForUserInDatabase(userID string, password string) (bool, userPermissions, error) {
+	// Search for userID and password in the users table and retrieve permissions
+	var permissions userPermissions
+	row := d.DB.QueryRow("SELECT can_read, can_write, can_delete FROM users WHERE user_id = $1 AND user_password = $2", userID, password)
+	err := row.Scan(&permissions.CanRead, &permissions.CanWrite, &permissions.CanDelete)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// Not actually an error, just means there was no match (user + password)
+			return false, userPermissions{}, nil
+		} else {
+			return false, userPermissions{}, err
+		}
+	}
+
+	return true, permissions, nil
+}
+
+func hasPermission(operation string, permissions userPermissions) bool {
+	switch operation {
+	case "READ":
+		if permissions.CanRead {
+			return true
+		}
+		return false
+	case "WRITE":
+		if permissions.CanWrite {
+			return true
+		}
+		return false
+	case "DELETE":
+		if permissions.CanDelete {
+			return true
+		}
+		return false
+	default:
+		return false
+	}
+}
+
+func (d database) authenticateAndAuthorize(userID string, userPassword string, operation string) (bool, bool, bool, error) {
+	authorized := false
+	accessGranted := false
+
+	authenticated, permissions, err := d.searchForUserInDatabase(userID, userPassword)
+	if err != nil {
+		return false, false, false, err
+	}
+
+	if authenticated {
+		authorized = hasPermission(operation, permissions)
+		if authorized {
+			accessGranted = true
+		}
+	}
+
+	return authenticated, authorized, accessGranted, nil
+}
--- gateway/gateway.go
+++ gateway/gateway.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -11,6 +12,24 @@ import (
 )
 
 func main() {
+	gin.SetMode(os.Getenv("GIN_MODE"))
+
+	d := database{
+		Host:     os.Getenv("CUSTOMER_SERVICE_DATABASE_HOST"),
+		Port:     os.Getenv("CUSTOMER_SERVICE_DATABASE_PORT"),
+		User:     os.Getenv("CUSTOMER_SERVICE_DATABASE_USER"),
+		Password: os.Getenv("CUSTOMER_SERVICE_DATABASE_PASSWORD"),
+		Name:     os.Getenv("CUSTOMER_SERVICE_DATABASE_NAME"),
+		DB:       &sql.DB{},
+	}
+
+	err := d.init()
+	if err != nil {
+		return
+	}
+
+	defer d.DB.Close()
+
 	router := gin.Default()
 
 	customerServiceURL := os.Getenv("CUSTOMER_SERVICE_URL")
@@ -18,6 +37,8 @@ func main() {
 	transactionServiceURL := os.Getenv("TRANSACTION_SERVICE_URL")
 	notificationServiceURL := os.Getenv("NOTIFICATION_SERVICE_URL")
 	balanceServiceURL := os.Getenv("BALANCE_SERVICE_URL")
+
+	gatewayAuth := os.Getenv("GATEWAY_AUTH")
 
 	// Define the target URLs for different paths
 	forwardURLs := map[string]string{
@@ -50,27 +71,63 @@ func main() {
 		proxies[path] = httputil.NewSingleHostReverseProxy(target)
 	}
 
-	// Define a handler that selects the appropriate reverse proxy based on the request path
-	forwardHandler := func(c *gin.Context) {
-		path := c.Request.URL.Path
-		proxy, ok := proxies[path]
-		if !ok {
-			log.Printf("Could not find endpoint for path %s", path)
-			c.JSON(http.StatusNotFound, gin.H{"error": "Endpoint not found"})
-			return
+	if gatewayAuth == "TRUE" {
+		// Define a handler which enforces auth before forwarding requests to the appropriate services
+		forwardHandler := func(c *gin.Context) {
+			path := c.Request.URL.Path
+			proxy, ok := proxies[path]
+			if !ok {
+				log.Printf("Could not find endpoint for path %s", path)
+				c.JSON(http.StatusNotFound, gin.H{"error": "Endpoint not found"})
+				return
+			}
+
+			// Extract userID and password from URL parameters
+			userID := c.Param("id")
+			userPassword := c.Param("password")
+
+			authenticated, authorized, accessGranted, err := d.authenticateAndAuthorize(userID, userPassword, "WRITE")
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			if accessGranted {
+				// Forward the request to the corresponding target URL
+				log.Printf("Received request at path %s, forwarding it to %s", path, forwardURLs[path])
+				proxy.ServeHTTP(c.Writer, c.Request)
+			} else {
+				c.JSON(http.StatusUnauthorized, gin.H{
+					"message":       "accessDenied",
+					"authenticated": authenticated,
+					"authorized":    authorized,
+				})
+			}
 		}
 
-		log.Printf("Received request at path %s e forwarded it to %s", path, proxies[path])
+		// Associate the forwardHandler with wildcard route
+		router.POST("/:userID/:password", forwardHandler)
 
-		// Forward the request to the corresponding target URL
-		proxy.ServeHTTP(c.Writer, c.Request)
+	} else {
+		// Define a basic handler that forwards requests to the appropriate services
+		forwardHandler := func(c *gin.Context) {
+			path := c.Request.URL.Path
+			proxy, ok := proxies[path]
+			if !ok {
+				log.Printf("Could not find endpoint for path %s", path)
+				c.JSON(http.StatusNotFound, gin.H{"error": "Endpoint not found"})
+				return
+			}
+
+			// Forward the request to the corresponding target URL
+			log.Printf("Received request at path %s, forwarding it to %s", path, forwardURLs[path])
+			proxy.ServeHTTP(c.Writer, c.Request)
+		}
+
+		// Associate the forwardHandler with wildcard route
+		router.POST("/*path", forwardHandler)
 	}
 
-	// Associate the forwardHandler with different paths
-	for path := range forwardURLs {
-		router.POST(path, forwardHandler)
-	}
-
-	// Run the Gin server on port 8000
+	// Run the Gin gateway on port 8000
 	router.Run(":8000")
 }
